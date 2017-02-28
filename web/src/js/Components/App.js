f4d0077b9cfa6b34fcdd04e7bd9b3d3731e3deda
Newtsrock <andrewtfluck@gmail.com>
2017-02-28 14:49:12 -0500

    didn't like the result all too much

--- web/src/js/Components/App.js
+++ web/src/js/Components/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Router, browserHistory } from 'react-router';
 import Routes from './Routes/';
 
-import consoleImage from 'console-image';
-
 export default class App extends React.Component {
     constructor() {
         super()
-        consoleImage('http://4vector.com/i/free-vector-stop-sign-clip-art_110930_Stop_Sign_clip_art_medium.png', 0.5);
-        console.log('%c This is a browser feature intended for developers. If someone told you to copy-paste something here to "hack" someone\'s Discord and StaffPortal account, it is a scam and will give them access to your Discord and StaffPortal account.', 'font-family: sans-serif; font-weight: bold; font-size: 24px; ');
+        console.log('%c Stop', 'font-family: sans-serif; font-weight: bold; font-size: 48px; color: red;')
+        console.log('%c This is a browser feature intended for developers. If someone told you to copy-paste something here to "hack" someone\'s StaffPortal account, it is a most obviously a scam and will give them access to your StaffPortal account.', 'font-family: sans-serif; font-weight: bold; font-size: 24px;');
     }
 
     render() {
