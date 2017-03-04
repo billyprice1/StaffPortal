9e3212a7c7dfc04730fb6e9ddbc545c90bbf4a31
Newtsrock <andrewtfluck@gmail.com>
2017-03-04 09:20:16 -0500

    Forgot 2 schemas :P

--- modules/database/schemas/role.js
+++ modules/database/schemas/role.js
@@ -1,10 +1,14 @@
-const mongoose = require('mongoose')
+import {Schema} from 'mongoose';
 
-const permissionSchema = require('./permission')
+import permissionSchema from './permission';
 
-module.exports = new mongoose.Schema({
-  permissions: [permissionSchema],
-  name: {type: String, required: true},
-  color: {type: String},
-  id: {type: String, unique: true, required: true}
-})
+export default new class Role extends Schema {
+  constructor() {
+    super({
+      permissions: [permissionSchema],
+      name: {type: String, required: true},
+      color: {type: String},
+      id: {type: String, unique: true, required: true}
+    });
+  }
+}
