b1996484a3fc6cf95cb51d278f186b4e2a4eb1f7
Newtsrock <andrewtfluck@gmail.com>
2017-03-05 10:32:11 -0500

    silly boi forgot to import colors

--- modules/log.js
+++ modules/log.js
@@ -1,4 +1,6 @@
+import colors from 'colors';
 import winston from 'winston';
+
 import clevels from './levelColors';
 
 export default new class Log extends winston.Logger {
@@ -14,9 +16,9 @@ export default new class Log extends winston.Logger {
           formatter: (options) => {
             let worker_id = options.meta.worker_id;
             if (options.level == 'crit') {
-              return (`(${options.timestamp()}) (Worker: ${worker_id ? worker_id : 'master'}) (${'CRITICAL'})` + ` ${options.message ? options.message : 'Unknown Critical Error Occured'}` + `${(options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '')}`+`\n\t\tStaffPortal can not continue and will shutdown.`);
+              return (`(${options.timestamp()}) (Worker: ${worker_id ? worker_id : 'master'}) (${'CRITICAL'.bgRed})` + ` ${options.message ? options.message : 'Unknown Critical Error Occured'}` + `${(options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '').red}`+`\n\t\tStaffPortal can not continue and will shutdown.`).bold;
             } else {
-              return `(${options.timestamp()}) (Worker: ${worker_id ? worker_id : 'master'}) (${options.level == 'err' ? 'ERROR' : options.level.toUpperCase()[clevels.colors[options.level]]})` + ` ${(options.message ? options.message : 'No message Specified')}` + `${(options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '')}` 
+              return `(${options.timestamp()}) (Worker: ${worker_id ? worker_id : 'master'}) (${options.level == 'err' ? 'ERROR'.red : options.level.toUpperCase()[clevels.colors[options.level]]})` + ` ${(options.message ? options.message : 'No message Specified')}` + `${(options.meta && Object.keys(options.meta).length ? '\n\t' + JSON.stringify(options.meta) : '')}`.gray 
             }
           },
           colorize: true,
