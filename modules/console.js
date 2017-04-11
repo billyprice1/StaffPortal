3952d6a36b553522b8fd34e9a05f6125fee5230b
Newtsrock <andrewtfluck@gmail.com>
2017-04-11 16:18:11 -0400

    reset new logger not in use, keeping others incase, but you should be a-ok

--- modules/console.js
+++ modules/console.js
@@ -1,14 +1,8 @@
-import winston from 'winston';
-import mkdirp from 'mkdirp';
-import colors from 'colors';
-import cluster from 'cluster';
-import os from 'os';
-
-import clevels from './levelColors';
-import logging from './log';
-
-colors.enabled = false;
-
+const winston = require("winston");
+const mkdirp = require('mkdirp');
+const colors = require('colors');
+const cluster = require('cluster');
+colors.enabled = true;
 if (cluster.isWorker) {
   logger = {
     crit: (msg, data) => {
@@ -32,14 +26,6 @@ if (cluster.isWorker) {
         message: msg,
         data: data,
         level: "warn"
-      }
-      cluster.worker.send(tosend)
-    },
-    info: (msg, data) => {
-      tosend = {
-        message: msg,
-        data: data,
-        level: "info"
       }
       cluster.worker.send(tosend)
     },
@@ -103,9 +89,10 @@ var logging = new (winston.Logger)({
   transports: [
     new (winston.transports.Console)({
       name: "console",
-      timestamp: () => {return new Date().toUTCString()},
+      timestamp: () => {return new Date().toDateString()},
       formatter: function(options) {
         var worker_id = options.meta.worker_id;
+        delete options.meta.worker_id;
         if (options.level == "crit") {
           return (`(${options.timestamp()}) (Worker: ${worker_id ? worker_id : "master"}) (${"CRITICAL".bgRed})` + ` ${options.message ? options.message : "Unknown Critical Error Occured"}` + `${(options.meta && Object.keys(options.meta).length ? "\n\t" + JSON.stringify(options.meta) : "").red}`+`\n\t\tStaffPortal can not continue and will shutdown.`).bold;
         } else {
@@ -113,13 +100,14 @@ var logging = new (winston.Logger)({
         }
       },
       colorize: true,
-      level: "info"
+      level: "warn"
     }),
     new (winston.transports.File)({
       name: "file-info",
-      timestamp: () => {return new Date().toUTCString()},
+      timestamp: () => {return new Date().toDateString()},
       formatter: function(options) {
         var worker_id = options.meta.worker_id;
+        delete options.meta.worker_id;
         if (options.level == "crit") {
           return (`(${options.timestamp()}) (Worker: ${worker_id ? worker_id : "master"}) (${"CRITICAL"})` + ` ${options.message ? options.message : "Unknown Critical Error Occured"}` + `${(options.meta && Object.keys(options.meta).length ? "\n\t" + JSON.stringify(options.meta) : "")}`+`\n\t\tStaffPortal can not continue and will shutdown.`);
         } else {
@@ -135,9 +123,10 @@ var logging = new (winston.Logger)({
     }),
     new (winston.transports.File)({
       name: "file-verbose",
-      timestamp: () => {return new Date().toUTCString()},
+      timestamp: () => {return new Date().toDateString()},
       formatter: function(options) {
         var worker_id = options.meta.worker_id;
+        delete options.meta.worker_id;
         if (options.level == "crit") {
           return (`(${options.timestamp()}) (Worker: ${worker_id ? worker_id : "master"}) (${"CRITICAL"})` + ` ${options.message ? options.message : "Unknown Critical Error Occured"}` + `${(options.meta && Object.keys(options.meta).length ? "\n\t" + JSON.stringify(options.meta) : "")}`+`\n\t\tStaffPortal can not continue and will shutdown.`);
         } else {
@@ -156,9 +145,10 @@ if (process.argv[2] == "DEBUG") {
   logging.add(
    new (winston.transports.File)({
      name: "file-exceptions",
-      timestamp: () => {return new Date().toUTCString()},
+      timestamp: () => {return new Date().toDateString()},
       formatter: function(options) {
         var worker_id = options.meta.worker_id;
+        delete options.meta.worker_id;
         if (options.level == "crit") {
           return (`(${options.timestamp()}) (Worker: ${worker_id ? worker_id : "master"}) (${"CRITICAL"})` + ` ${options.message ? options.message : "Unknown Critical Error Occured"}` + `${(options.meta && Object.keys(options.meta).length ? "\n\t" + JSON.stringify(options.meta) : "")}`+`\n\t\tStaffPortal can not continue and will shutdown.`);
         } else {
@@ -180,58 +170,12 @@ logging.on('error', function (err) {
 });
 winston.addColors(clevels.colors);
 if (cluster.isMaster) {
-  worker_ui_ready = 0;
-  worker_agent_ready = 0;
-  worker_db_ready = 0;
   cluster.on("message", (worker, msg, hande) => {
-    if (!msg.type || msg.type == "log") {
-      msg.data.worker_id = worker.id
-      logging[msg.level](msg.message, msg.data);
-      if (msg.level == "crit") {
-        process.exit(1)
-      }
-    } else if (msg.type == "status") {
-      switch(msg.subject) {
-        case "web":
-          if (msg.data == "ready") {
-            worker_ui_ready++
-            logging.logging("Worker interface is listening for incoming connections", {"worker_id":worker.id})
-            if (worker_ui_ready == os.cpus().length) {
-              logging.info("Web Interface started successfully");
-            }
-          } else if (msg.data == "error") {
-            logging.warn("A worker has suffered an error while attempting to start the Web Interface. Worker has been restared.", {"worker_id":worker.id,"err_name":msg.err.name,"err_message":msg.err.message});
-          } else {
-            logging.logging("Worker sent unknown message data, message is ignored",{"worker_id":worker.id,"msg_data":msg.data})
-          }
-          break;
-        case "agent":
-          if (msg.data == "ready") {
-            worker_agent_ready++
-            logging.logging("Worker started Agent", {"worker_id":worker.id})
-            if (worker_agent_ready == os.cpus().length) {
-              logging.info("Third-Party Agent started successfully!")
-            }
-          } else if (msg.data == "error") {
-            logging.warn("A worker has suffered an error while attempting to start the Web Interface. Worker has been restared.", {"worker_id":worker.id,"err_name":msg.err.name,"err_message":msg.err.message});
-          } else {
-            logging.logging("Worker sent unknown message data, message is ignored",{"worker_id":worker.id,"msg_data":msg.data})
-          }
-          break;
-        case "db":
-          if (msg.data == "ready") {
-            worker_db_ready++
-            logging.logging("Worker connected to DB")
-            if (worker_db_ready == os.cpus().length) {
-              logging.info("All workers are connected to MongoDB")
-            }
-          } else {
-            logging.logging("Worker sent unknown message data, message is ignored",{"worker_id":worker.id,"msg_data":msg.data})
-          }
-          break;
-        default:
-          logging.logging("Worker sent unknown message subject, message is ignored",{"worker_id":worker.id,"msg_subject":msg.subject})
-      }
+    msg.data.worker_id = worker.id
+    logging[msg.level](msg.message, msg.data);
+    if (msg.level == "crit") {
+      process.exit(1)
     }
   })
 }
+module.exports = logging
